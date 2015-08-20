568afeecd1c27e0d52527765afd050b99c22d41c
Raindolf Owusu <iraindolf@yahoo.com>
2015-08-20 13:16:42 -0400

    edited app.js

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ var homeController = require('./controllers/home');
 var userController = require('./controllers/user');
 var apiController = require('./controllers/api');
 var contactController = require('./controllers/contact');
+var learnController = require( './controllers/learn')
 
 /**
  * API keys and Passport configuration.
@@ -95,6 +96,7 @@ app.use(express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }))
 app.get('/', homeController.index);
 app.get('/login', userController.getLogin);
 app.post('/login', userController.postLogin);
+app.get('/learn', learnController.getlearn);
 app.get('/logout', userController.logout);
 app.get('/forgot', userController.getForgot);
 app.post('/forgot', userController.postForgot);
